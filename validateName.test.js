93741bb1b9bae7698030c2214e7e813bdf14daaf
newthiagoassisk8 <assisthiago2303@gmail.com>
2026-01-05 16:46:31 -0300

    teste de validacao de nomes agora garantindo espacamento

--- validateName.test.js
+++ validateName.test.js
@@ -1,24 +1,24 @@
-//Regra: Validação se o nome recebido é uma string e a primeira letra é maiscula
-let failFirst = true;
+//Regra: Validação se o nome recebido é uma string e a primeira letra é maiscula e se não há espacamento
 
 let namesToTest = [
     ['Fernanda', true],
     ['Thiago', true],
     ['thiago', false],
-    [undefined, false][('fernanda', false)],
-    // [' tiago', false]
-    // [' ', false]
+    [' tiago', false],
+    [undefined, false],
+    ['fernanda', false],
+    [' ', false]
 ];
 
 function isValidName(name) {
-    return typeof name === 'string' && name[0] === name[0].toUpperCase();
+    return typeof name === 'string' && name[0] === name[0].toUpperCase() && name === name.trim();
 }
 
 function testingNames(namesList) {
     for (let i = 0; i < namesList.length; i++) {
         let [value, expected] = namesList[i];
         let result = isValidName(value);
-        const isAssert = result === expected;
+        this.isAssert = result === expected;
         console.log({ value, expected, result, isAssert });
     }
     if (!isAssert) {
