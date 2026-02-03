c824f4e1f84c5c6c082bc2332fd75e398502ccb7
newthiagoassisk8 <assisthiago2303@gmail.com>
2026-02-03 08:35:00 -0300

    try catch statement added

--- sum.js
+++ sum.js
@@ -1,4 +1,12 @@
 function sum(num1, num2) {
-    return num1 + num2;
+    try {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            throw new Error('must be a number');
+        }
+        return num1 + num2;
+    } catch (e) {
+        return e;
+    }
 }
+
 export default sum;
